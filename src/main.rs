666083680b6382e40e3621c39f5b14e8d98b07af
Raphael Gstettenhofer <UmbraAdMortem8@gmail.com>
2026-07-04 01:00:43 +0200

    feat(cli): Add working directory argument and update directory handling

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,19 @@ struct Args {
     /// Number of times to greet
     #[arg(short, long, default_value_t = 1)]
     count: u8,
+    
+    /// Set working directory
+    #[arg(short, long, default_value = ".")]
+    workdir: std::path::PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
+
+    println!("Current dir: {:?}", std::env::current_dir());
+    println!("Change dir:  {:?}", args.workdir);
+    std::env::set_current_dir(args.workdir);
+    println!("Changed dir: {:?}", std::env::current_dir());
 
     for _ in 0..args.count {
         println!("Hello, {}!", args.name);
