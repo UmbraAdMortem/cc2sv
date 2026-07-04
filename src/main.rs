0a9276264648bd92ee219e34335d6d10f4276884
Raphael Gstettenhofer <UmbraAdMortem8@gmail.com>
2026-07-04 21:12:58 +0200

    feat: Update main function to test ownership movement and references

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s = "Hello, World!";
+
+    let mut s1 = String::from(s);
+    s1.push_str(s);
+
+    let s2 = &s1;
+
+    println!("{s}");
+    println!("{s1}");
+    println!("{s2}");
+
 }
