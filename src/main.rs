7975e1b03041c24c44bf138e27481934b4ae4269
Raphael Gstettenhofer <UmbraAdMortem8@gmail.com>
2026-07-04 11:17:06 +0200

    refactor(cli): Improve error handling and code readability in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// Exits function on error
+// use anyhow::{Context, Ok, Result};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -8,28 +10,58 @@ use clap::Parser;
     long_about = "Conventional Commits 2 Semantic Versions\n\nA language agnostic CLI tool written in rust for generating semantic versions and change-logs by parsing conventional commits."
 )]
 struct Args {
-    /// The name to greet
+    // The name to greet
     #[arg(short, long, default_value = "World")]
     name: String,
 
-    /// Number of times to greet
+    // Number of times to greet
     #[arg(short, long, default_value_t = 1)]
     count: u8,
-    
-    /// Set working directory
+
+    // Set working directory
     #[arg(short, long, default_value = ".")]
     workdir: std::path::PathBuf,
 }
 
+// Exits function on error
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+// Continues function on error
 fn main() {
+    // TODO: Refactor cli  handling
     let args = Args::parse();
 
-    println!("Current dir: {:?}", std::env::current_dir());
-    println!("Change dir:  {:?}", args.workdir);
-    std::env::set_current_dir(args.workdir);
-    println!("Changed dir: {:?}", std::env::current_dir());
+    println!(
+        "Current dir: {}",
+        std::env::current_dir()
+            .expect("Current dir should be valid")
+            .display()
+    );
+    println!("Change dir:  {}", &args.workdir.display());
+
+    // Exits function on error
+    // std::env::set_current_dir(&args.workdir)
+    //     .with_context(|| format!("\nMissing dir: {}", &args.workdir.display()))?;
+    // println!("Changed dir: {}", &args.workdir.display());
+
+    // Continues function on error
+    match std::env::set_current_dir(&args.workdir) {
+        Ok(_) => {
+            println!(
+                "Changed dir: {}",
+                std::env::current_dir()
+                    .expect("Current dir should be valid")
+                    .display()
+            );
+        }
+        Err(_) => {
+            eprintln!("Missing dir: {}", &args.workdir.display());
+        }
+    }
 
     for _ in 0..args.count {
         println!("Hello, {}!", args.name);
     }
+
+    // Exits function on error
+    // Ok(())
 }
